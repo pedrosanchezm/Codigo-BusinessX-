2cd4ee55bb9b0574ad0fb57fbc90739829d50299
PedroSanchez <pedroasm_20@hotmail.com>
2019-01-14 10:41:24 -0600

    Nueva solucion agregada

---
+++ ProyectoBusinessX/BusinessX.Entities/Libro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessX.Entities
+{
+    public class Libro
+    {
+        //Tercer comentario
+        public int IDL { get; set; }
+        public string NombreL { get; set; }
+        public string AutorL { get; set; }
+        public string Editorial { get; set; }
+        public string ISBN { get; set; }
+        public double PrecioL { get; set; }
+        public string RutaImgL { get; set; }
+        public int IDGeneroL { get; set; }
+    }
+}
