50ab55cf83c75964fa783cc3b0024343414d6492
mauriciormr <mauricio.martinez@korinver.com>
2019-01-16 10:08:56 -0600

    ModificacionBibliotecaDAL
    Agregando los elementos de la tabla T_Libros

--- PatternRepository/PatternRepository.DAL/BibliotecaDAL.cs
+++ PatternRepository/PatternRepository.DAL/BibliotecaDAL.cs
@@ -20,13 +20,13 @@ namespace PatternRepository.DAL
 
         public List<Libro> GetLibros()
         {
-            var query = "SELECT * FROM \"T_Alumnos\"";
+            var query = "SELECT * FROM \"T_Libros\"";
             return repository.GetAll(query).ToList();
         }
 
         public List<Libro> InsertLibros(Libro libro)
         {
-            var query = "INSERT INTO \"T_Alumnos\" (NombreAlum,ApellidoAlum,DireccionAlum,TelefonoAlum) ";
+            var query = "INSERT INTO \"T_Libros\" (NombreL,AutorL,Editorial,ISBN,PrecioL,IDGeneroL) ";
             query+= string.Format("VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6});", libro.NombreL, libro.AutorL, libro.Editorial, libro.ISBN, libro.PrecioL, libro.RutaImgL, libro.IDGeneroL);
             return repository.GetAll(query).ToList();
         }
